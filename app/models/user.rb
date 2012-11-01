53e40023f8d22e801d964444300ead1f434e83ee
Andrew <andrew8088@gmail.com>
2012-11-01 17:58:16 -0400

    created user resourse

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  attr_accessible :email, :name, :password, :username
+
+  has_secure_password
+
+  validates :email, uniqueness: true, presence: true, format: { with: /[\w\.+-]+@([\w]+\.)+\w+/ }
+  validates :username, uniqueness: true, presence: true
+  validates :name, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Ribbit::Application.routes.draw do
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
