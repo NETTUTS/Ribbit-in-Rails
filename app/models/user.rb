d8967f836b892537f099b82c3bd9b2de6e509a15
Andrew <andrew8088@gmail.com>
2012-11-02 06:55:48 -0400

    added avatars from Gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,24 @@
+require 'digest/md5'
+
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password, :password_confirmation, :username
+  before_validation :prep_email
+  before_save :create_avatar_hash
+
+  attr_accessible :email, :name, :password, :password_confirmation, :username, :avatar_hash
 
   has_secure_password
 
-  validates :email, uniqueness: true, presence: true, format: { with: /[\w\.+-]+@([\w]+\.)+\w+/ }
+  validates :email, uniqueness: true, presence: true, format: { with: /^[\w\.+-]+@([\w]+\.)+\w+$/ }
   validates :username, uniqueness: true, presence: true
   validates :name, presence: true
+
+  private
+
+  def prep_email
+    self.email = self.email.strip.downcase
+  end
+
+  def create_avatar_hash
+    self.avatar_hash = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
+  end
 end
