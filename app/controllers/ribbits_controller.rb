8a7fe32505edfbb930ed2d4648e3552b0d8d1642
Andrew <andrew8088@gmail.com>
2013-01-02 15:00:43 -0500

    we can ribbit from the profile page!

--- app/controllers/ribbits_controller.rb
+++ app/controllers/ribbits_controller.rb
@@ -1,2 +1,13 @@
 class RibbitsController < ApplicationController
+    def create
+        @ribbit = Ribbit.new(params[:ribbit])
+        @ribbit.user_id = current_user.id
+
+        if @ribbit.save
+            redirect_to current_user 
+        else
+            flash[:error] = "Problem!";
+            redirect_to current_user
+        end
+    end
 end
--- app/models/ribbit.rb
+++ app/models/ribbit.rb
@@ -1,4 +1,7 @@
 class Ribbit < ActiveRecord::Base
-  attr_accessible :content, :user_id
-  belongs_to :user
+    default_scope order: 'created_at DESC'
+    attr_accessible :content, :user_id
+    belongs_to :user
+
+    validates :content, length: { maximum: 140 }
 end
